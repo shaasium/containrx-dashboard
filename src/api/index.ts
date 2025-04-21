7ff49b969ca3e30f7d1d7bea6c751dc49ec3762d
Tanmay Vaij <tanmayvaij22@gmail.com>
2025-04-22 00:09:32 +0530

    hardcoded backendurl
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const containrxApi = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
+  baseURL: `http://localhost:5000/api`,
 });
 
 export const signIn = async (email: string, password: string) => {
