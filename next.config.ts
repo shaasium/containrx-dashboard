4016981ae8aae141ea413d256a989616588e2e05
Tanmay Vaij <tanmayvaij22@gmail.com>
2025-04-23 20:43:16 +0530

    changes in env name
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  env: {
-    BACKEND_URL: process.env.BACKEND_URL
-  }
 };
 
 export default nextConfig;
