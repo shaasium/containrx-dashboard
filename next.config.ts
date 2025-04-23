d5e7181bb03c5d108c5bc881edf12cd3e4117be5
Tanmay Vaij <tanmayvaij22@gmail.com>
2025-04-23 20:25:30 +0530

    added env in config
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone"
+  output: "standalone",
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL
+  }
 };
 
 export default nextConfig;
